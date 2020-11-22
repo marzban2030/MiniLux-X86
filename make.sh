49a83e99c4dff3d2739c23c24f6ea9e2455abc2e
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2020-11-22 21:36:23 +0330

    Create make.sh

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 wget https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-i686
 
 #Step2: Download latest famous linux Kernel from ubuntu official website.
-wget http://archive.ubuntu.com/ubuntu/dists/bionic-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux
+wget http://archive.ubuntu.com/ubuntu/dists/bionic-updates/main/installer-i386/current/images/netboot/ubuntu-installer/i386/linux
 
 #Step3: Download latest famous linux distribution base archived file (Ubuntu 18.04 LTS) from its official website.
 wget http://cdimage.ubuntu.com/ubuntu-base/releases/18.04/release/ubuntu-base-18.04.5-base-i386.tar.gz
